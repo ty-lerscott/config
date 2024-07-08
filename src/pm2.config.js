701c020f2a7de30ba16b50a0ef03fca8a9f55de4
Tyler Scott <ty@lerscott.com>
2024-07-08 00:27:50 -0400

    updating tarballing

--- src/pm2.config.js
+++ src/pm2.config.js
@@ -1,16 +1,18 @@
+const path = require('path');
+
+const cwd = process.cwd();
+
 module.exports = {
     apps: [
         {
             name: 'canopy',
-            cwd: "./canopy",
+            cwd: path.resolve(cwd, "..", "..", "canopy"),
             script: 'npm',
             args: "start",
             instances: '2', // Or a specific number of instances
-            exec_mode: 'cluster', // Enables clustering mode
+            exec_mode: 'cluster',
             env: {
-                NODE_ENV: 'production',
                 PORT: 3000,
-                // Add other environment variables here
             },
         },
     ],
