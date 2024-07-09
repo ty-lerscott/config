add53d964afce842ccad744ef9bb1f9b1554e39a
Tyler Scott <ty@lerscott.com>
2024-07-09 00:22:20 -0400

    update canopy pm2 config

--- src/pm2.config.js
+++ src/pm2.config.js
@@ -1,12 +1,11 @@
 const path = require('path');
 
-const cwd = process.cwd();
 
 module.exports = {
     apps: [
         {
             name: 'canopy',
-            cwd: path.resolve(cwd, "..", "..", "canopy"),
+            cwd: path.resolve(__dirname, "..", "..", "canopy"),
             script: 'npm',
             args: "start",
             instances: '2', // Or a specific number of instances
@@ -14,6 +13,7 @@ module.exports = {
             env: {
                 PORT: 3000,
             },
+            ignore_watch: ["node_modules", "logs", "src"],
         },
     ],
 };
