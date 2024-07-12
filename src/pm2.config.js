e5cb5ea5b247a45701e2804d4d7bf9ec06e7ec5a
Tyler Scott <ty@lerscott.com>
2024-07-12 16:20:13 -0400

    Fix pm2.config

--- src/pm2.config.js
+++ src/pm2.config.js
@@ -7,7 +7,7 @@ module.exports = {
             name: 'canopy',
             cwd: path.resolve(__dirname, "..", "..", "canopy"),
             script: 'npm',
-            args: "start",
+            args: "start:all",
             instances: '1',
             exec_mode: 'fork',
             env: {
